b4a0185186385ed1e4acfb1405754eea528ee525
marianacrmz <marianacramirez.18@gmail.com>
2024-08-14 19:37:49 -0600

    Se agregó bd

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersecretkey'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///library.db'
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://biblioteca_user:password@localhost/biblioteca'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwtsecretkey'
+    JWT_SECRET_KEY = 'your_jwt_secret_key'
+
 
